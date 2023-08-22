6b9decae5769d16d47a64bd952cc24a8476211ee
Mateo Gutierrez <mgutierrezca@unal.edu.co>
2023-08-22 17:29:17 -0500

    web scraping

---
+++ linkedin_education_spider/linkedin_education_spider/settings.py
@@ -0,0 +1,43 @@
+# Scrapy settings for linkedin_education_spider project
+#
+# For simplicity, this file contains only settings considered important or
+# commonly used. You can find more settings consulting the documentation:
+#
+#     https://docs.scrapy.org/en/latest/topics/settings.html
+#     https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
+#     https://docs.scrapy.org/en/latest/topics/spider-middleware.html
+
+BOT_NAME = "linkedin_education_spider"
+
+SPIDER_MODULES = ["linkedin_education_spider.spiders"]
+NEWSPIDER_MODULE = "linkedin_education_spider.spiders"
+
+# HTTPCACHE_ENABLED = True
+
+# Obey robots.txt rules
+ROBOTSTXT_OBEY = False
+
+
+
+SCRAPEOPS_API_KEY = '48d6cd4d-3476-414f-8fb4-f64539330026'
+
+SCRAPEOPS_PROXY_ENABLED = True
+
+# Add In The ScrapeOps Monitoring Extension
+EXTENSIONS = {
+'scrapeops_scrapy.extension.ScrapeOpsMonitor': 500, 
+}
+
+
+DOWNLOADER_MIDDLEWARES = {
+
+    ## ScrapeOps Monitor
+    'scrapeops_scrapy.middleware.retry.RetryMiddleware': 550,
+    'scrapy.downloadermiddlewares.retry.RetryMiddleware': None,
+    
+    ## Proxy Middleware
+    'scrapeops_scrapy_proxy_sdk.scrapeops_scrapy_proxy_sdk.ScrapeOpsScrapyProxySdk': 725,
+}
+
+# Max Concurrency On ScrapeOps Proxy Free Plan is 1 thread
+CONCURRENT_REQUESTS = 1
