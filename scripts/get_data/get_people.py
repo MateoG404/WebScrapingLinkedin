b58b1adc58c1eb4bf8f596e73324b339cc9e15f9
MateoG404 <mgutierrezca@unal.edu.co>
2023-09-15 20:03:38 -0500

    Create script get data linkeding using data base UN

---
+++ scripts/get_data/get_people.py
@@ -0,0 +1,21 @@
+from pandas import pandas
+
+
+class Graduates : 
+
+    def __init__(self):
+
+        name__ = str
+        email_ = str
+        bachelor_ = str
+
+    
+    def get_data_from_csv(self,name,email,bachelor):
+
+        self.email_ = email
+        self.name_ = name 
+        self.bachelor_ = bachelor
+
+    
+        
+print("hello")
---
+++ scripts/process_data/process_csv_university.py
@@ -0,0 +1,18 @@
+import pandas as pd
+
+class File :
+
+    def __init__(self, Path):
+        
+        self.PATH = Path 
+        self.file = pd.read_excel(self.PATH,sheet_name='Sheet2')
+        self.num_engineering = int
+
+
+    def get_faculty(self,fac_name):
+
+        print(self.file.info())        
+
+arhivos = File('/home/mateosky/Desktop/MateoCodes/WebScrapingLinkedin/documentacion/RE_EGR_ACR_TABLA DE DATOS 2022007.xlsx')
+
+arhivos.get_faculty('ingenieria')
