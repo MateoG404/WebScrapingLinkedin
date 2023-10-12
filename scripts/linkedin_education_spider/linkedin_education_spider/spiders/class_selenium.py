229b1c386329f43e2e648a4a6f5d286022d32656
Mateo Gutierrez <mgutierrezca@unal.edu.co>
2023-10-12 08:48:31 -0500

    Nuevo scraper

---
+++ scripts/linkedin_education_spider/linkedin_education_spider/spiders/class_selenium.py
@@ -0,0 +1,73 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
+import csv
+from dotenv import load_dotenv
+import pandas as pd
+import os
+
+class Busqueda:
+
+    def __init__(self) -> None:
+        load_dotenv()
+        options = Options()
+        options.add_argument('--disable-dev-shm-usage')
+        options.add_argument('--incognito')
+        self.driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+
+    def login_with_email(self):
+        driver = self.driver
+        email = driver.find_element(By.XPATH,'//*[@id="username"]')
+        email.send_keys(os.environ.get('EMAIL2'))
+        password = driver.find_element(By.XPATH,'//*[@id="password"]')
+        password.send_keys(os.environ.get('PASSWORD'))
+        self.sesion = driver.find_element(By.XPATH,'//*[@id="organic-div"]/form/div[3]/button').click()
+        #time.sleep(60)
+
+    def scrape_profile(self, profile_url):
+        
+        driver = self.driver
+        
+        driver.get(profile_url)
+        time.sleep(3)
+        
+        profile_data = {}
+        
+        # Extraer nombre
+        name = driver.find_element_by_css_selector('.inline.t-24.t-black.t-normal.break-words').text
+        profile_data['name'] = name
+        
+        # Extraer descripción
+        description = driver.find_element_by_css_selector('.mt1.t-18.t-black.t-normal.break-words').text
+        profile_data['description'] = description
+        
+        # Extraer ubicación
+        location = driver.find_element_by_css_selector('.t-16.t-black.t-normal.inline-block').text
+        profile_data['location'] = location
+        
+        # Extraer seguidores
+        followers = driver.find_element_by_css_selector('.align-self-center.t-16.t-black--light').text
+        profile_data['followers'] = followers
+        
+        # Extraer conexiones
+        connections = driver.find_element_by_css_selector('.pv-top-card--list.pv-top-card--list-bullet.mt1').text
+        profile_data['connections'] = connections
+        
+        # Aquí puede continuar con la extracción de más campos como 'about', 'experience', 'education', etc.
+        
+        return profile_data
+
+    def abrir_navegador(self):
+        self.url = "https://linkedin.com/uas/login"
+        self.driver.get(self.url)
+        self.login_with_email()
+
+    def iniciar(self):
+        self.abrir_navegador()
---
+++ scripts/linkedin_education_spider/linkedin_education_spider/spiders/selenium_linkedin_people.py
@@ -0,0 +1,73 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
+import csv
+from dotenv import load_dotenv
+import pandas as pd
+import os
+
+from class_selenium import Busqueda
+from linkedin_links import LinkedinLinks
+
+def login_to_linkedin(driver, username, password):
+    driver.get("https://www.linkedin.com/login")
+    time.sleep(2)
+    
+    driver.find_element_by_id("username").send_keys(username)
+    driver.find_element_by_id("password").send_keys(password)
+    driver.find_element_by_id("password").send_keys(Keys.RETURN)
+    time.sleep(3)
+
+def scrape_profile(driver, profile_url):
+    driver.get(profile_url)
+    time.sleep(3)
+    
+    # Aquí puede añadir el código para extraer la información específica que necesita
+    name = driver.find_element_by_css_selector('.inline.t-24.t-black.t-normal.break-words').text
+    return {"name": name}
+
+if __name__ == "__main__":
+    busqueda_obj = Busqueda()
+    busqueda_obj.iniciar()
+
+    #print("buscando ..", nombre)
+    busqueda_obj.driver.get("https://www.google.com")
+    busqueda_google = busqueda_obj.driver.find_element(By.NAME, "q")
+    busqueda_google.clear()
+
+    objeto_linkedin = LinkedinLinks()
+    lista_links = objeto_linkedin.get_links('/home/user/Desktop/MateoCodes/WebScrapingLinkedin/documentacion/NEW_DATA/clean_people_get_link.xlsx')
+    
+    lista_links = lista_links[500:502]
+    
+    for profile in lista_links:
+        busqueda_obj.scrape_profile(profile)
+    
+    print(lista_links)
+
+'''
+
+    busqueda_google.send_keys(nombre)
+    busqueda_google.send_keys(Keys.RETURN)
+'''
+
+'''
+    
+    driver = webdriver.Chrome(executable_path="/path/to/chromedriver")
+    login_to_linkedin(driver, "your_username", "your_password")
+    
+    profile_urls = ["https://www.linkedin.com/in/example1/", "https://www.linkedin.com/in/example2/"]
+    for url in profile_urls:
+        info = scrape_profile(driver, url)
+        print(info)
+    
+    driver.quit()
+
+'''
